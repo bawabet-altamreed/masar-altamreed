99ac7230a2d3b23b93fc7d713d71a8492c76237d
bawabet-altamreed <medo.beso2023@gmail.com>
2026-08-28 08:26:37 +0300

    Create auth.js

---
+++ src/firebase/auth.js
@@ -0,0 +1,52 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    updateProfile
+} from "firebase/auth";
+
+import { app } from "./config.js";
+
+const auth = getAuth(app);
+
+export async function registerUser(email, password, displayName) {
+    const userCredential =
+        await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+    const user = userCredential.user;
+
+    if (displayName) {
+        await updateProfile(user, {
+            displayName: displayName
+        });
+    }
+
+    return user;
+}
+
+export async function loginUser(email, password) {
+    const userCredential =
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+    return userCredential.user;
+}
+
+export async function logoutUser() {
+    await signOut(auth);
+}
+
+export function observeAuth(callback) {
+    return onAuthStateChanged(auth, callback);
+}
+
+export { auth };
