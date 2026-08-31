2a4547081974052ee3ddcc705aaf133edb112e93
bawabet-altamreed <medo.beso2023@gmail.com>
2026-08-31 11:36:31 +0300

    Delete functions.js

--- functions.js
+++
@@ -1,4 +0,0 @@
-import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-functions.js";
-import { app } from "./firebase.js";
-export const functions = getFunctions(app);
-export const call = (name, data) => httpsCallable(functions, name)(data);
